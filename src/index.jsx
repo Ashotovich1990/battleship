d057fab0abc6d14f669b2242187309bd7260eb12
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-08-09 15:18:00 +0400

    set up webpack and babel

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const root = document.getElementById("root"); 
+    ReactDOM.render(<h1>My wife left me because of React</h1>, root);
+})
