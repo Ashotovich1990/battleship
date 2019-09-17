de4fca9967681fbb9dc7961c3ba2babc069768cd
Aram Sargsyan <aramsargsyan990@gmail.com>
2019-09-17 14:50:48 +0400

    style board squares

--- src/game/square.jsx
+++ src/game/square.jsx
@@ -4,18 +4,27 @@ class Square extends React.Component {
     constructor(props) {
         super(props);
         this.content = this.content.bind(this);
+        this.state = {style: "hidden"}
     }
 
     content() {
-        if (this.props.value === 2) return "X";
-        if (this.props.value === 3) return "(Y)";
-        // if (this.props.value === 5) return "PPP";
-        return "O";
+        // if (this.props.value === 2) return "X";
+        // if (this.props.value === 3) return "(Y)";
+
+        // if (this.props.value === 1) return "PP";
+        // this.setState({style: "empty-cell"});
+    }; 
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.value !== this.props.value) {
+            if (this.props.value === 2) this.setState({style: "missed"});
+            if (this.props.value === 3) this.setState({style: "strike"});
+        };
     }
 
     render () {
-        return <li>{this.content()}</li>
-    }
-}
+        return <li className={this.state.style}>{this.content()}</li>
+    };
+};
 
 export default Square;
